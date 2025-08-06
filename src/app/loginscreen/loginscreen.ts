5f6ea9d0a844bb6fecc170effa82a39ddec7d293
Arul <arul.paul.85@gmail.com>
2025-08-06 18:10:45 +0530

    Added a login screen- basic.

--- src/app/loginscreen/loginscreen.ts
+++ src/app/loginscreen/loginscreen.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loginscreen',
-  imports: [],
   templateUrl: './loginscreen.html',
-  styleUrl: './loginscreen.scss'
+  styleUrl: './loginscreen.scss',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  providers: [FormBuilder]
 })
 export class Loginscreen {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      remember: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
